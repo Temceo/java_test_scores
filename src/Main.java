f2dcc1ad3be2a6b3c250dba8468c64f0760b4858
Martin Mwaka <martinmwaka@hotmail.com>
2025-10-31 00:01:59 +0000

    add start here statement

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Enter test scores between 0 and 100");
         System.out.println("To exit type \"x\"");
         System.out.println("====================================");
+
+        System.out.println("Start here...");
     }
 }
